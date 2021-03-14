c20dd5e6b13d817f0f4667006ef400fa9884db0f
Suziny <78532880+Suziny91@users.noreply.github.com>
2021-03-15 02:18:51 +0900

    Add files via upload

---
+++ MyGreet.java
@@ -0,0 +1,20 @@
+package greet;
+
+public class MyGreet {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+
+	}//main
+
+}//class
+/*
+ * 01.프로젝트의 문제점
+ * ::
+ * Hello Java라는 인삿말만 출력하는 코드가 생산된다.
+ * 다른 인삿말을 출력하려면 다시 코드를 수정해야 한다.
+ * --> 유지보수성, 재사용성 낮은 코드
+ * 
+ */
+
+
